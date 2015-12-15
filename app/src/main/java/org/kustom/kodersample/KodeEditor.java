0dd83cfa6676cd17b77ca141d19009e5584588bf
Frank Monza <frank.bmonza@gmail.com>
2015-12-15 18:01:41 +0100

    added extra existing kode example

--- app/src/main/java/org/kustom/kodersample/KodeEditor.java
+++ app/src/main/java/org/kustom/kodersample/KodeEditor.java
@@ -1,12 +1,12 @@
 package org.kustom.kodersample;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-public class KodeEditor extends AppCompatActivity implements View.OnClickListener{
+public class KodeEditor extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +14,13 @@ public class KodeEditor extends AppCompatActivity implements View.OnClickListene
         setContentView(R.layout.activity_kode_editor);
         // When Send Kode button is clicked we send the data back to Kustom
         findViewById(R.id.kode_send).setOnClickListener(this);
+        // Set current text from source activity and change it a bit
+        if (getIntent() != null) {
+            String text = getIntent().getStringExtra("org.kustom.extra.KODE");
+            if (text != null) {
+                ((TextView) findViewById(R.id.kode_edit)).setText(text + " EDITED!");
+            }
+        }
     }
 
     @Override
